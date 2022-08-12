cf101d8401636819d14a9a4818256771fe27021d
pengbuster <ethantnguyen@gmail.com>
2022-08-11 20:46:28 -0700

    Workin

---
+++ Image-Compressor.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+
+
+namespace main
+{
+    public class imageCompressor
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("ok");
+            Bitmap testImage = new Bitmap(testImage.jpg);
+        }
+    }
+}
