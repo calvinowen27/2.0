fc70d5fbd6288c211dc2a9fb5697f3eee8e224e6
pengbuster <ethantnguyen@gmail.com>
2022-08-11 21:11:47 -0700

    Fixing issue

--- Image-Compressor.cs
+++ Image-Compressor.cs
@@ -1,13 +1,21 @@
-﻿using System.Drawing;
+﻿using System;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 
+class ImageCompressor {
+        static void Main(string[] args) {
+            var path = @"../test.png";
+            var img = Image.Load<Rgba32>(path);
+            
+            var height = img.Height;
+            var width = img.Width;
 
-namespace main
-{
-    public class imageCompressor
-    {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("ok");
+            for(int y = 0; y < height; y++) {
+                for(int x = 0; x < width; x++) {
+                    var px = img[x, y];
+                    var rgb = new int[] { px.R, px.G, px.B };
+                    Console.WriteLine(rgb[0]);
+                }
+            }
         }
     }
-}
